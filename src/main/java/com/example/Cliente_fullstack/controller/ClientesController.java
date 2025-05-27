461145bd241eaa881b2943cafbcc2ccb9a3d1467
Fer-Don-oso <fer.donosoc@duocuc.cl>
2025-05-26 22:24:50 -0400

    "cambios hechos"

--- src/main/java/com/example/Cliente_fullstack/controller/ClientesController.java
+++ src/main/java/com/example/Cliente_fullstack/controller/ClientesController.java
@@ -3,14 +3,19 @@ package com.example.Cliente_fullstack.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.Cliente_fullstack.Model.Cliente;
 import com.example.Cliente_fullstack.service.ClienteService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
@@ -27,7 +32,48 @@ public class ClientesController {
             if (Cliente.isEmpty()) {
                 return ResponseEntity.noContent().build();
             }
-            return ResponseEntity.ok();
+            return ResponseEntity.ok(Cliente);
         }
-        
+        @PostMapping
+        public ResponseEntity<Cliente> guardar(@RequestBody Cliente cliente) {
+            Cliente clienteNuevo = ClienteService.agregarCliente(cliente);
+            return ResponseEntity.status(HttpStatus.CREATED).body(clienteNuevo);
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Cliente> buscar(@PathVariable Integer id) {
+            try {
+                Cliente cliente = ClienteService.buscarporID(id);
+                return ResponseEntity.ok(cliente);
+            } catch (Exception e ) {
+                return ResponseEntity.notFound().build();
+            }
+        }
+
+        @PutMapping("/{id}")
+        public ResponseEntity<Cliente> actualizar(@PathVariable Integer id, @RequestBody Cliente cliente) {
+            try {
+                Cliente cli = ClienteService.buscarporID(id);
+                cli.setNombre(cliente.getNombre());
+                cli.setApellido_paterno(cliente.getApellido_paterno());
+                cli.setApellido_materno(cliente.getApellido_materno());
+                cli.setCorreo(cliente.getCorreo());
+                cli.setFechaNacimiento(cliente.getFechaNacimiento());
+                cli.setTelefono(cliente.getTelefono());
+
+                ClienteService.agregarCliente(cli);
+                return ResponseEntity.ok(cliente);
+            } catch (Exception e) {
+                return ResponseEntity.notFound().build();
+            }
+        }
+        @DeleteMapping("/{id}") 
+        public ResponseEntity<?> eliminar(@PathVariable Long id) {
+            try {
+                ClienteService.eliminarCliente(id);
+                return ResponseEntity.noContent().build();
+            } catch (Exception e) {
+                return ResponseEntity.notFound().build();
+            }
+        }
 }
