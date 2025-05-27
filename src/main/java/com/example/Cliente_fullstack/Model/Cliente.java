f841962f0c21a87332e83926b78f4ea9b8d18be3
Fer-Don-oso <fer.donosoc@duocuc.cl>
2025-05-26 23:39:52 -0400

    ya no se lo que hago

--- src/main/java/com/example/Cliente_fullstack/Model/Cliente.java
+++ src/main/java/com/example/Cliente_fullstack/Model/Cliente.java
@@ -2,7 +2,12 @@ package com.example.Cliente_fullstack.Model;
 
 import java.util.Date;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
